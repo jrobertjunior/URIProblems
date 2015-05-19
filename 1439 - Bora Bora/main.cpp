89f41923fd6c8e0ba074122958adbf7b4976fc0f
jose <jrobert.junior@gmail.com>
2015-05-18 23:23:31 -0300

    Bora bora, fixed, running in 0.000s on URI site

--- 1439 - Bora Bora/main.cpp
+++ 1439 - Bora Bora/main.cpp
@@ -1,18 +1,46 @@
+/**
+ * @autor Jose Junior
+ * 1439 - Bora Bora
+ * Runtime : 0.000s
+ */
+
 #include <iostream>
-#include <list>
 #include <vector>
+#include <list>
 #include <algorithm>
+
+// Just to debug
+const char cHeart[]         = "\xe2\x99\xa5";
+const char cClub[]      	= "\xe2\x99\xa3";
+const char cSpade[]         = "\xe2\x99\xa0";
+const char cDiamond[]   	= "\xe2\x99\xa6";
+
 
 using namespace std;
 
 struct card
 {
-    short   rank;
-    char    suit;
-    card()
+    short rank;
+    char suit;
+
+    card(): rank(0), suit(0){}
+
+    // Just to debug
+    void printCard()
     {
-        rank = 0;
-        suit = 0;
+        if(rank > 0 && suit > 0)
+        {
+            const char* symbol = (suit == 'H') ? cHeart : (suit == 'C') ?  cClub : (suit == 'S') ? cSpade : cDiamond;
+            cout << "[";
+            if (rank > 1 && rank < 11){ cout << rank; }
+            else if (rank == 1) {   cout << 'A'; }
+            else if (rank == 11){   cout << 'J'; }
+            else if (rank == 12){   cout << 'Q'; }
+            else if (rank == 13){   cout << 'K'; }
+
+            cout << symbol;
+            cout << "]";
+        }
     }
 
     bool isBlanc()
@@ -24,26 +52,46 @@ struct card
         return false;
     }
 
-    bool operator<(const card& c) const
+    bool operator<(const card& card) const
     {
-        if(this->rank == c.rank)
+        if(this->rank == card.rank)
         {
-            return suit < c.suit;
+            return suit < card.suit;
         }
-        return this->rank < c.rank;
+        return this->rank < card.rank;
     }
 
-    bool operator==(const card& c) const
+    bool operator==(const card& card) const
     {
-        if(this->rank == c.rank &&
-                this->suit == c.suit)
+        if(this->rank == card.rank &&
+                this->suit == card.suit)
         {
             return true;
         }
         return false;
     }
-
 };
+
+// Just to debug
+void printDeck(vector<card> cards)
+{
+   for(vector<card>::iterator it = cards.begin(); it!=cards.end(); ++it)
+   {
+       it->printCard(); cout << " ";
+   }
+   cout << endl;
+}
+
+// Just to debug
+void printDeck(list<card> cards)
+{
+   for(list<card>::iterator it = cards.begin(); it!=cards.end(); ++it)
+   {
+       it->printCard(); cout << " ";
+   }
+   cout << endl;
+}
+
 
 struct player
 {
@@ -52,7 +100,7 @@ struct player
     list<card> hearts;
     list<card> spades;
 
-    inline bool isEmpty()
+    bool isHandEmpty()
     {
         if(clubs.size() || diamonds.size() || hearts.size() || spades.size())
         {
@@ -61,9 +109,8 @@ struct player
         return true;
     }
 
-    inline void DrawnCard(card newCard)
+    void addCard(card newCard)
     {
-
         if (newCard.suit == 'C')
         {
             clubs.push_back(newCard);
@@ -86,7 +133,18 @@ struct player
         }
     }
 
-    inline card topNumber(short number, list<card> cards)
+    void DrawnCard(vector<card> *stack, int numberOfCards)
+    {
+
+        for(int i = 0 ; i < numberOfCards; i++)
+        {
+            card newCard  = stack->back();
+            stack->pop_back();
+            addCard(newCard);
+        }
+    }
+
+    card findSameRank(short number, list<card> cards)
     {
         card ret;
         for(list<card>::iterator it = cards.begin(); it!=cards.end(); ++it)
@@ -105,7 +163,7 @@ struct player
         {
             if(it->rank == number)
             {
-                cards->remove(*it);
+                cards->erase(it);
                 break;
             }
         }
@@ -113,113 +171,107 @@ struct player
 
     card DiscardCard(card topCard)
     {
-        card tempCard;
+        card discardedCard;
         if (topCard.suit == 'C' && clubs.size() > 0)
         {
-            tempCard = clubs.back();
-            if(tempCard.rank > topCard.rank)
+            discardedCard = clubs.back();
+            if(discardedCard.rank > topCard.rank)
             {
                 clubs.pop_back();
-                return tempCard;
+                return discardedCard;
             }
         }
         else if (topCard.suit == 'D' && diamonds.size() > 0)
         {
-            tempCard = diamonds.back();
-            if(tempCard.rank > topCard.rank)
+            discardedCard = diamonds.back();
+            if(discardedCard.rank > topCard.rank)
             {
                 diamonds.pop_back();
-                return tempCard;
+                return discardedCard;
             }
         }
         else if (topCard.suit == 'H' && hearts.size() > 0)
         {
-            tempCard = hearts.back();
-            if(tempCard.rank > topCard.rank)
+            discardedCard = hearts.back();
+            if(discardedCard.rank > topCard.rank)
             {
                 hearts.pop_back();
-                return tempCard;
+                return discardedCard;
             }
         }
         else if (topCard.suit == 'S' && spades.size() > 0)
         {
-            tempCard = spades.back();
-            if(tempCard.rank > topCard.rank)
+            discardedCard = spades.back();
+            if(discardedCard.rank > topCard.rank)
             {
                 spades.pop_back();
-                return tempCard;
+                return discardedCard;
             }
         }
 
-        card findMe = topNumber(topCard.rank, spades);
+        card findMe = findSameRank(topCard.rank, spades);
         if(findMe.rank == 0)
         {
-            findMe = topNumber(topCard.rank, hearts);
+            findMe = findSameRank(topCard.rank, hearts);
             if(findMe.rank == 0)
             {
-                findMe = topNumber(topCard.rank, diamonds);
+                findMe = findSameRank(topCard.rank, diamonds);
                 if(findMe.rank == 0)
                 {
-                    findMe = topNumber(topCard.rank, clubs);
+                    findMe = findSameRank(topCard.rank, clubs);
                 }
             }
         }
 
-        if(findMe.rank > tempCard.rank)
+        if(findMe.rank > discardedCard.rank)
         {
-        	tempCard = findMe;
+            discardedCard = findMe;
         }
-        else if(findMe.rank == tempCard.rank)
+        else if(findMe.rank == discardedCard.rank)
         {
-        	if(findMe.suit > tempCard.suit)
-        	{
-        		tempCard = findMe;
-        	}
-        }
-
-        if(tempCard.suit > 0)
-        {
-        	remove(tempCard.rank,
-        			(tempCard.suit == 'D') ? &diamonds :
-        					(tempCard.suit == 'S') ? &spades :
-        							(tempCard.suit == 'C') ? &clubs : &hearts
-					);
+            if(findMe.suit > discardedCard.suit)
+            {
+                discardedCard = findMe;
+            }
         }
 
+        if(discardedCard.suit > 0)
+        {
+            if(discardedCard.suit == 'D')     { remove( discardedCard.rank,&diamonds);}
+            else if(discardedCard.suit == 'S'){ remove( discardedCard.rank,&spades  );}
+            else if(discardedCard.suit == 'C'){ remove( discardedCard.rank,&clubs   );}
+            else if(discardedCard.suit == 'H'){ remove( discardedCard.rank,&hearts  );}
+        }
 
-        return tempCard;
+        return discardedCard;
     }
+
+    // Just to debug
+    void printHand()
+    {
+         cout << "|-------------------------------------------------------\\" << endl;
+         cout << "|clubs    :";    printDeck(clubs);
+         cout << "|diamonds :";    printDeck(diamonds);
+         cout << "|hearts   :";    printDeck(hearts);
+         cout << "|spades   :";    printDeck(spades);
+         cout << "|-------------------------------------------------------/" << endl;
+    }
+
 };
 
-void printDeck(list<card> cards)
+inline int getNextPlayer(int player, int direction, int players)
 {
-   for(list<card>::iterator it = cards.begin(); it!=cards.end(); ++it)
-   {
-	   cout << "[" << it->rank << "] ";
-   }
-}
-
-void printDeck(vector<card> cards)
-{
-   for(vector<card>::iterator it = cards.begin(); it!=cards.end(); ++it)
-   {
-	   cout << "[" << it->rank << "] ";
-   }
-}
-
-inline int getNextPlayer(int player, int nextPlayer, int players)
-{
-    if(player == 0 && nextPlayer == -1)
+    if(player == 1 && direction == -1)
     {
-        player = players-1;
+        player = players;
     }
-    else if (player == players-1 && nextPlayer == 1)
+    else if (player == players && direction == 1)
     {
-        player = 0;
+        player = 1;
     }
     else
     {
-        player += nextPlayer;
+        player += direction;
     }
     return player;
 }
@@ -230,8 +282,6 @@ int main()
     cin.tie(NULL);
 
     int players, hand, deck;
-
-
     while(cin >> players)
     {
         if (players == 0)
@@ -240,18 +290,18 @@ int main()
         }
         cin >> hand >> deck;
 
-        player *playersList = new player[players];
+        player *playersList = new player[players+1];
         vector<card> discardPile;
         vector<card> stockPile;
 
         // reading cards for players
-        for(int i = 0; i < players; i++)
+        for(int i = 1; i <= players; i++)
         {
             for(int j = 0; j < hand; j++)
             {
                 card c;
                 cin >> c.rank >> c.suit;
-                playersList[i].DrawnCard(c);
+                playersList[i].addCard(c);
             }
         }
         card c;
@@ -267,107 +317,72 @@ int main()
         }
 
         reverse(stockPile.begin(),stockPile.end());
-        int winner = 0;
-        int player = 0;
-        int nextPlayer = 1;
-        bool discaded = true;
-        while(!winner)
+
+        int winnerPlayer    = 0;
+        int directionOfPlay = (discardPile.back().rank == 12) ? -1 : 1; // in case of an initial queen the game starts with a c-cw;
+        int currentPlayer   = 1;    //always start from the player 1
+        bool discardedCard  = true; //count the first card in the discard pile as a discarded card action
+        int round = 1;
+        while (!winnerPlayer)
         {
-//        	cout << "--------------------------------------------------------------------" << endl;
-//        	cout << "current player:: " << player << endl;
-//        	cout << "current top [" << discardPile.back().rank << "|" << discardPile.back().suit << "]" << endl;
-//        	cout << "deck: "; printDeck(stockPile); cout << endl;
-////        	for(int pp = 0; pp < players; pp++)
-////        	{
-//        		//cout << "p: " << pp << endl;
-//        		cout << "clubs: \t"; printDeck(playersList[player].clubs); cout << endl;
-//        		cout << "diamonds: \t"; printDeck(playersList[player].diamonds); cout << endl;
-//        		cout << "spades: \t"; printDeck(playersList[player].spades); cout << endl;
-//        		cout << "hearts: \t"; printDeck(playersList[player].hearts); cout << endl;
-//        		cout << endl;
-////        	}
-//        	cout << "--------------------------------------------------------------------" << endl;
-            if(discaded)
+            // First Action: see the last discarded card in my turn
+            card discardTop = discardPile.back();
+
+            if (discardedCard)
             {
-                card discardTop = discardPile.back();
-                discaded = false;
-
-                if(discardTop.rank == 12)
+                discardedCard = false;
+                switch (discardTop.rank)
                 {
-                    nextPlayer *= -1;
-                }
-                else if (discardTop.rank == 7)
-                {
-                    playersList[player].DrawnCard(stockPile.back());
-                    stockPile.pop_back();
-
-                    playersList[player].DrawnCard(stockPile.back());
-                    stockPile.pop_back();
-
-                    player = getNextPlayer(player, nextPlayer, players);
-                    continue;
-                }
-                else if (discardTop.rank == 1)
-                {
-                    playersList[player].DrawnCard(stockPile.back());
-                    stockPile.pop_back();
-                    player = getNextPlayer(player, nextPlayer, players);
-                    continue;
-
-                }
-                else if (discardTop.rank == 11)
-                {
-                    player = getNextPlayer(player, nextPlayer, players);
-                    continue;
+                    case 1:  //Ace, draw one card and skip play
+                        playersList[currentPlayer].DrawnCard(&stockPile, 1);
+                        currentPlayer = getNextPlayer(currentPlayer, directionOfPlay, players);
+                        continue;
+                    case 7:  // 7, draw two card and skip play
+                        playersList[currentPlayer].DrawnCard(&stockPile,2);
+                        currentPlayer = getNextPlayer(currentPlayer, directionOfPlay, players);
+                        continue;
+                    case 11: // J, skip play
+                        currentPlayer = getNextPlayer(currentPlayer, directionOfPlay, players);
+                        continue;
                 }
             }
 
+            // Player see if he can discard a card;
+            card discarded = playersList[currentPlayer].DiscardCard(discardTop);
+            if (discarded.isBlanc())
+            {
+                // no card to discard, buy one more
+                playersList[currentPlayer].DrawnCard(&stockPile, 1);
+                discarded = playersList[currentPlayer].DiscardCard(discardTop);
+            }
 
-            card discarded = playersList[player].DiscardCard(discardPile.back());
-
-            if ( discarded.rank != 0)
+            if (!discarded.isBlanc())
             {
                 discardPile.push_back(discarded);
                 if(discarded.rank != 12)
                 {
-                	discaded = true;
+                    discardedCard = true;
                 }
                 else
                 {
-                	discaded = false;
-                	nextPlayer *= -1;
-                }
-            }
-            else
-            {
-                playersList[player].DrawnCard(stockPile.back());
-                stockPile.pop_back();
-                card discarded = playersList[player].DiscardCard(discardPile.back());
-                if ( discarded.rank != 0)
-                {
-                    discardPile.push_back(discarded);
-                    if(discarded.rank != 12)
-					{
-						discaded = true;
-					}
-					else
-					{
-						discaded = false;
-						nextPlayer = -1;
-					}
+                    // is a Queen, change the direction
+                    directionOfPlay *= -1;
+                    discardedCard = false;
                 }
             }
 
-
-            if (playersList[player].isEmpty())
+            //Test for winner!!!!;
+            if(playersList[currentPlayer].isHandEmpty())
             {
-                winner = player+1;
+                winnerPlayer = currentPlayer;
+                cout << winnerPlayer << endl;
             }
             else
             {
-                player = getNextPlayer(player, nextPlayer, players);
+                currentPlayer = getNextPlayer(currentPlayer, directionOfPlay, players);
             }
+
         }
-        cout << winner << endl;
     }
+    return 0;
 }
