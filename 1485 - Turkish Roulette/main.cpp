45ceb9dc2d4ef0a6dca70112f21e2ebe179fbfb6
Jose <jrr.junior@samsung.com>
2015-05-29 09:19:38 -0300

    Roulette Final

--- 1485 - Turkish Roulette/main.cpp
+++ 1485 - Turkish Roulette/main.cpp
@@ -1,107 +1,117 @@
+/**
+ * 1485 - Turkish Roulette
+ * @Autor José Junior
+ *
+ * Runtime: 0.120s
+ * 
+ * Date: 2015.05.29
+ */
+
 #include <iostream>
+#include <cstring>
 #include <sstream>
-
-using namespace std;
-
-int memory[126][251];
-
-void findMax(int ball, int slot, int ballMax, int slotMax, int max, int *result )
+ 
+// Nice way to exit the loop
+inline bool nextTest(int *sSlots, int *bBalls)
 {
-    if (ball < ballMax)
-    {
-        for (int i = slot; i < slotMax + slot - 2; i++)
-        {
-            int slt = i % (slotMax-1);
-            int mem = memory[ball][slt];
-            findMax(ball+1, i+2, ballMax, slotMax, max + mem, result);
-
-        }
-
-    }
-    if(*result < max)
-    {
-        *result = max;
-
-    }
-
+     std::cin >> *sSlots >> *bBalls;
+     if(*sSlots == 0 && *bBalls == 0)
+     {
+         return false;
+     }
+     std::cin.ignore();
+     return true;
 }
-
-
+ 
+inline void getIntLine(int array[])
+{
+    std::string input;
+    std::getline(std::cin, input);
+    std::stringstream line(input);
+    int next;
+    int i = 1;
+    while (line >> next)
+    {
+        array[i++] = next;
+    }
+}
+ 
+inline void getIntLineCircle(int array[], int size)
+{
+    std::string input;
+    std::getline(std::cin, input);
+    std::stringstream line(input);
+    int next;
+    int i = 1;
+    while (line >> next)
+    {
+        array[i] = -next;
+        array[size + i++] = -next;
+    }
+}
+ 
 int main()
 {
+    // Make it fast in c++
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+ 
     int sSlots, bBalls;
-    string line;
-
-    while (true)
+ 
+    while (nextTest(&sSlots, &bBalls))
     {
-        cin >> sSlots >> bBalls;
-        if (!sSlots) break;
+        int slots[2*(sSlots+1)];
+        int balls[bBalls+1];
+        int memory[sSlots+1][sSlots+1];
+        int best = -2000000000;
+        int points;
 
-        int slots[sSlots];
+        getIntLineCircle(slots, sSlots);
+        getIntLine(balls);
 
-        cin.ignore();
-        getline(cin, line);
-
-        cout << "lineSlots: " << line << endl;
-
-        stringstream roulette(line);
-
-        getline(cin, line);
-        stringstream balls(line);
-
-        cout << "lineBalls: " << line << endl;
-
-        roulette >> slots[0] >> slots[1];
-        int first = slots[0];
-        for (int i = 0; i < sSlots-1; i++)
+        // As the Roulette is a circle list, we need to test for the first ball starting in different positions
+        // This way the other balls can occupy this space in another round, and all balls in all slots will be verified
+        for (int round = 0; round < sSlots; round++)
         {
-            slots[i] += slots[i+1];
-            roulette >> slots[i+2];
-
-        }
-        slots[sSlots-1] += first;
-
-        for (int i = 0; i < sSlots; i++)
-        {
-            cout << slots[i] << " ";
-
-        }
-        cout << endl;
-
-        for (int i = 0; i < bBalls; i++)
-        {
-            int ball;
-            balls >> ball;
-            for (int j = 0; j < sSlots; j++)
+            for (int i = 1; i <= bBalls; i++)
             {
-                memory[i][j] =  slots[j] * ball;
+                for (int j = 2 * i; j <= sSlots; j++)
+                {
+                    // Calculates the points of the place where the ball is.
+                    points = (slots[j + round] + slots[ (j - 1) + round]) * balls[i];
 
+                    if (j == 2 * i)
+                    {
+                        memory[i][j] = points;
+                        if (i > 1)
+                        {
+                            memory[i][j] += memory[i - 1][j - 2];
+                        }
+ 
+                    }
+                    else
+                    {
+                        memory[i][j] = memory[i][j - 1];
+ 
+                        if (i > 1)
+                        {
+                            points += memory[i - 1][j - 2];
+                        }
+                        if (points > memory[i][j])
+                        {
+                            memory[i][j] = points;
+                        }
+ 
+                    }
+                }
             }
-
-        }
-        cout << endl;
-
-        for (int i = 0; i < bBalls; i++)
-        {
-            for (int j = 0; j < sSlots; j++)
+            if (memory[bBalls][sSlots] > best)
             {
-                cout <<  memory[i][j] << " ";
-
+                best = memory[bBalls][sSlots];
             }
-            cout << endl;
-
         }
-        cout << endl;
-
-        int result = 0;
-        findMax(0,0,bBalls, sSlots, 0, &result);
-        cout << result << endl;
-
+        std::cout << best << "\n";
     }
-
-
+    std::cout.flush();
     return 0;
-
 }
-
-
